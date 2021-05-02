8d1bfddf0d29294997dfa99cd6547f5495f687aa
louischarlot <51994892+louischarlot@users.noreply.github.com>
2021-05-02 12:32:30 +0200

    Update Code_methode_1.R

--- Codes/Code_methode_1.R
+++ Codes/Code_methode_1.R
@@ -117,6 +117,12 @@ phtest.formula <- function(x, data, model = c("within", "random"),
            reX <- model.matrix(re_mod, cstcovar.rm = "intercept")
            feX <- model.matrix(fe_mod, cstcovar.rm = "all")
            
+           
+           
+           
+           
+           
+           
            dimnames(feX)[[2]] <- paste(dimnames(feX)[[2]], "tilde", sep=".")
            ## estimated models could have fewer obs (due dropping of NAs) compared to the original data
            ## => match original data and observations used in estimated models
@@ -221,6 +227,68 @@ feX <- model.matrix(fe_mod, cstcovar.rm = "all")
 
 
 
+
+
+
+
+dimnames(feX)[[2]] <- paste(dimnames(feX)[[2]], "tilde", sep=".")
+
+## estimated models could have fewer obs (due dropping of NAs) compared to the original data
+## => match original data and observations used in estimated models
+## routine adapted from lmtest::bptest
+commonrownames <- intersect(intersect(intersect(row.names(data), names(reY)), row.names(reX)), row.names(feX))
+if (!(all(c(row.names(data) %in% commonrownames, commonrownames %in% row.names(data))))) {
+  data <- data[commonrownames, ]
+  reY  <- reY[commonrownames]
+  reX  <- reX[commonrownames, ]
+  feX  <- feX[commonrownames, ]
+}
+
+# Tests of correct matching of obs (just for safety ...)
+if (!all.equal(length(reY), nrow(data), nrow(reX), nrow(feX)))
+  stop("number of cases/observations do not match, most likely due to NAs in \"data\"")
+if (any(c(is.na(names(reY)), is.na(row.names(data)), is.na(row.names(reX)), is.na(row.names(feX)))))
+  stop("one (or more) rowname(s) is (are) NA")
+if (!all.equal(names(reY), row.names(data), row.names(reX), row.names(feX)))
+  stop("row.names of cases/observations do not match, most likely due to NAs in \"data\"")
+
+## fetch indices here, check pdata
+## construct data set and formula for auxiliary regression
+data <- pdata.frame(cbind(index(data), reY, reX, feX))
+auxfm <- as.formula(paste("reY~",
+                          paste(dimnames(reX)[[2]],
+                                collapse="+"), "+",
+                          paste(dimnames(feX)[[2]],
+                                collapse="+"), sep=""))
+auxmod <- plm(formula = auxfm, data = data, model = "pooling")
+nvars <- dim(feX)[[2]]
+R <- diag(1, nvars)
+r <- rep(0, nvars) # here just for clarity of illustration
+omega0 <- vcov(auxmod)[(nvars+2):(nvars*2+1),
+                       (nvars+2):(nvars*2+1)]
+Rbr <- R %*% coef(auxmod)[(nvars+2):(nvars*2+1)] - r
+
+h2t <- as.numeric(crossprod(Rbr, solve(omega0, Rbr)))
+ph2t <- pchisq(h2t, df = nvars, lower.tail = FALSE)
+
+df <- nvars
+names(df) <- "df"
+names(h2t) <- "chisq"
+
+if (!is.null(vcov)) {
+  vcov <- paste(", vcov: ",
+                paste(deparse(substitute(vcov))),
+                sep="")
+}
+
+haus2 <- list(statistic   = h2t,
+              p.value     = ph2t,
+              parameter   = df,
+              method      = paste("Regression-based Hausman test", vcov, sep=""),
+              alternative = "one model is inconsistent",
+              data.name   = paste(deparse(substitute(x))))
+class(haus2) <- "htest"
+return(haus2)
 
 
 
